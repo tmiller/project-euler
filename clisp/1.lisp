e9a1502ccc20f1b3d455ac4fe8f8e1e63b3e210d
Tom Miller <tom.miller@luckie.com>
2011-08-16 15:08:31 -0500

    Re-tabbing problem 1

--- clisp/1.lisp
+++ clisp/1.lisp
@@ -1,6 +1,7 @@
 (let ((total 0))
-  (dotimes (n 1001)
-    (when (or (= 0 (rem n 5)) (= 0 (rem n 3)))
-      (setq total (+ total n))))
-  (prin1 total))
+     (dotimes (n 1001)
+              (when (or (= 0 (rem n 5))
+                        (= 0 (rem n 3)))
+                    (setq total (+ total n))))
+     (prin1 total))
 
